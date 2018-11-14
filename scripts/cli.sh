b2d4fdcba4be558948d0089d443ebe58e6f25ebd
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-11-14 18:22:13 +0200

    Improve cleaning procedure

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -5,11 +5,13 @@
 handle_argument () {
     case "${1}" in
         "clean")
+            rm "${root_dir}/stages/built.txt" >/dev/null || true
+
             echo ">>> Removing built stage tarballs"
             find "${root_dir}/stages" -type f -name "finished.tar*" -print -delete || true
 
             echo ">>> Removing '${root_dir}/tmp'"
-            rm -rf "${root_dir}/tmp"
+            rm -rf "${root_dir}/tmp" >/dev/null || true
             ;;
         "exit")
             exit 0
