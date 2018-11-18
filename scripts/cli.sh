8233dcd8fbae70643036e80a603ccd06dbda7909
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-11-18 23:51:42 +0200

    Add help text

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -4,6 +4,24 @@
 
 handle_argument () {
     case "${1}" in
+        "help"|"-h"|"--help")
+            cat <<EOF
+musl_root - Yet another Musl-based lightweight container or distribution bootstrapper
+
+Usage:
+    ${_cred}${_cbold}help, -h, --help${_cnormal}
+        Show this help text and exits
+
+    ${_cred}${_cbold}clean${_cnormal}
+        Cleans up '${_cblue}${root_dir}/tmp${_cnormal}' and built stage tarballs
+
+    ${_cred}${_cbold}exit${_cnormal}
+        Exits script immediately after processing command line arguments.
+        Note that this ${_cred}*must*${_cnormal} be the last argument, as arguments are processed
+        in order.
+EOF
+            exit 0
+            ;;
         "clean")
             rm "${root_dir}/stages/built.txt" >/dev/null || true
 
